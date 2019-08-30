a3c504577de3f7e9555c8134bce94377d1660212
Alexander Belov <asb@rdw.ru>
2019-08-30 20:57:04 +0300

    [ci skip] Updates

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,3 +1,3 @@
 var typedoc = typedoc || {};
             typedoc.search = typedoc.search || {};
-            typedoc.search.data = {"kinds":{"64":"Function"},"rows":[{"id":0,"kind":64,"name":"isPowerOfTwo","url":"globals.html#ispoweroftwo","classes":"tsd-kind-function"}]};
+            typedoc.search.data = {"kinds":{"64":"Function"},"rows":[{"id":0,"kind":64,"name":"isPowerOfTwo","url":"globals.html#ispoweroftwo","classes":"tsd-kind-function"},{"id":1,"kind":64,"name":"nearestHighestPowerOfTwo","url":"globals.html#nearesthighestpoweroftwo","classes":"tsd-kind-function"}]};
