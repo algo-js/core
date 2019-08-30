03375f5ff6092764143753710efab2e68cbc827f
Alexander Belov <asb@rdw.ru>
2019-08-30 20:56:55 +0300

    Fixes

--- src/lib/math/nearest-highest-power-of-two/nearest-highest-power-of-two.spec.ts
+++ src/lib/math/nearest-highest-power-of-two/nearest-highest-power-of-two.spec.ts
@@ -4,21 +4,20 @@ import { nearestHighestPowerOfTwo } from './nearest-highest-power-of-two';
 
 test('nearest highest power of two', t => {
   const tests = [
-    [ 1, 0 ],
-    [ 2, 1 ],
-    [ 3, 2 ],
-    [ 5, 3 ],
-    [ 11, 4 ],
-    [ 1023, 10 ],
-    [ 1024, 10 ],
-    [ 1025, 11 ],
-    [ 2 ** 31 - 1, 31 ],
-    [ 2 ** 78 - 1, 78 ],
-    [ 2 ** 78 + 1, 79 ],
+    [1, 0],
+    [2, 1],
+    [3, 2],
+    [5, 3],
+    [11, 4],
+    [1023, 10],
+    [1024, 10],
+    [1025, 11],
+    [2 ** 31 - 1, 31],
+    [2 ** 78 - 1, 78]
   ];
 
   for (const v of tests) {
-    const [ value, power ] = v;
+    const [value, power] = v;
 
     t.is(nearestHighestPowerOfTwo(value), power);
   }
